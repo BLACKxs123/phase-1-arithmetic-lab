c8769e59adfdb74a06593a843ccf24fe4ffe3ac8
BLACKxs123 <fonteevans123@gmail.com>
2024-07-15 22:33:08 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Challenge 1: Multiply Variables
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply); // Output: 62
 
-// Write your code here
+// Challenge 2: Generate Random Integer
+const random = Math.floor(Math.random() * 10) + 1; // Generates random integer between 1 and 10
+console.log(random); // Output: Random integer between 1 and 10
+
+// Challenge 3: Calculate Remainder
+let num3 = 40;
+let num4 = 9;
+const mod = num3 % num4;
+console.log(mod); // Output: 4
+
+// Challenge 4: Find Maximum Number
+const max = Math.max(1, 2, 3, 4, 5, 20);
+console.log(max); // Output: 20
+
+
